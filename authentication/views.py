f4eedd9b9ed047dd4fa2dceb0f178467da71c91d
aditonorman <muhammad.raditya33@ui.ac.id>
2024-10-09 11:31:55 +0700

    splitted auth module

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from main.models import FoodEntry
+from django.http import HttpResponse
+from django.core import serializers
+from main.models import FoodEntry  # Import your model
+from main.forms import FoodEntryForm  # Import your form
+import os
+import csv
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-# Create your views here.
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('auth:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+   if request.method == 'POST':
+      form = AuthenticationForm(data=request.POST)
+
+      if form.is_valid():
+        user = form.get_user()
+        login(request, user)
+        response = HttpResponseRedirect(reverse("main:show_main"))
+        response.set_cookie('last_login', str(datetime.datetime.now()))
+        return response
+
+   else:
+      form = AuthenticationForm(request)
+   context = {'form': form}
+   return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('auth:login'))
+    response.delete_cookie('last_login')
+    return response
--- midterm_project/settings.py
+++ midterm_project/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'authentication',
+    'leaderboards',
+    'user_role',
 ]
 
 MIDDLEWARE = [
