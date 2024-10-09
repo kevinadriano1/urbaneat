78040696bc24c63d8ea420a4bacceb8ea4c3b6fa
Kevin Adriano <kevin.adriano@cs.ui.ac.id>
2024-10-09 20:50:49 +0700

    debug

--- midterm_project/settings.py
+++ midterm_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g*&jn%iz!+imy+fur9!02l#c**zrpp%z@we)(nz%df40$icg9!
 
 # SECURITY WARNING: don't run with debug turned on in production!
 PRODUCTION = os.getenv("PRODUCTION", False)
-DEBUG = True
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1","kevin-adriano-urbaneat.pbp.cs.ui.ac.id"]
 
