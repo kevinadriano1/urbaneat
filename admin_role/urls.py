a2970dc68d96a7e6ca59c3775a45171fbca9d58e
Mcflurrins <flori.andrea.60@gmail.com>
2024-12-17 00:08:49 +0700

    Fixed bugs related to creating new restos

--- admin_role/urls.py
+++ admin_role/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from admin_role.views import edit_restaurant, create_restaurant, delete_restaurant_via_ajax
+from admin_role.views import edit_restaurant, create_restaurant, delete_restaurant_via_ajax, edit_restaurant_api,\
+create_restaurant_api, delete_restaurant_api
 app_name = 'admin_role' 
 urlpatterns = [
     path('edit_resto/<uuid:id>/', edit_restaurant, name='edit_restaurant'),
     path('create_resto/', create_restaurant, name='create_restaurant'),
     path('delete_resto_ajax/<uuid:id>/', delete_restaurant_via_ajax, name='delete_restaurant_via_ajax'),
+    path('edit_resto_api/<uuid:id>/', edit_restaurant_api, name='edit_restaurant_api'),
+    path('create_resto_api/', create_restaurant_api, name='create_restaurant_api'),
+    path('delete_resto_api/<uuid:id>/', delete_restaurant_api, name='delete_restaurant_api'),
 
 ]
--- admin_role/views.py
+++ admin_role/views.py
@@ -5,6 +5,65 @@ from .forms import FoodEntryForm
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+
+@csrf_exempt
+def edit_restaurant_api(request, id):
+    if request.method == 'POST':
+        restaurant_entry = get_object_or_404(FoodEntry, id=id)
+        try:
+            # Parse incoming JSON data
+            data = json.loads(request.body)
+            # Update the restaurant object with the received data
+            form = FoodEntryForm(data, instance=restaurant_entry)
+            if form.is_valid():
+                form.save()
+                return JsonResponse({'message': 'Restaurant updated successfully.'}, status=200)
+            else:
+                # Return form errors
+                return JsonResponse({'error': 'Invalid data.', 'details': form.errors}, status=400)
+
+        except json.JSONDecodeError:
+            return JsonResponse({'error': 'Invalid JSON.'}, status=400)
+        
+    return JsonResponse({'message': 'Invalid request method'}, status=405)
+        
+@csrf_exempt
+def create_restaurant_api(request):
+    if request.method == 'POST':
+        try:
+            # Parse JSON data
+            data = json.loads(request.body)
+            form = FoodEntryForm(data)
+            if form.is_valid():
+                food_entry = form.save(commit=False)
+                food_entry.reviews_rating = 0.0
+                food_entry.number_of_reviews = 0
+                food_entry.avg_rating = "0.0"
+                food_entry.save()
+                return JsonResponse({
+                    'message': 'Restaurant created successfully',
+                }, status=200)
+            else:
+                return JsonResponse({'errors': form.errors}, status=400)
+        except json.JSONDecodeError:
+            return JsonResponse({'message': 'Invalid JSON data'}, status=400)
+
+    return JsonResponse({'message': 'Invalid request method'}, status=405)
+
+@csrf_exempt
+def delete_restaurant_api(request, id):
+    if request.method == 'DELETE':
+        try:
+            food_entry = get_object_or_404(FoodEntry, id=id)
+            food_entry.delete()
+            return JsonResponse({'message': 'Restaurant deleted successfully'})
+        except FoodEntry.DoesNotExist:
+            return JsonResponse({'message': 'Restaurant not found'}, status=404)
+
+    return JsonResponse({'message': 'Invalid request method'}, status=405)
+
 
 # Custom decorator to check if user is in the Restaurant_Manager group
 def is_restaurant_manager(user):
@@ -54,6 +113,7 @@ def create_restaurant(request):
             food_entry = form.save(commit=False)
             food_entry.reviews_rating = 0.0
             food_entry.number_of_reviews = 0
+            food_entry.avg_rating = "0.0"
             food_entry.user = request.user  # Associate the current user with the entry
             food_entry.save()
             # Redirect to the main view or another relevant page after creation
